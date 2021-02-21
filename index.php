8dac8b051c9dad02df1027be5aa02c50582f6812
Krishna <krishjan95@gmail.com>
2021-02-21 10:39:00 +0530

    with sql file

--- index.php
+++ index.php
@@ -1,13 +1,71 @@
 <?php
+// Initialize the session
+session_start();
+ 
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: home.php");
+    exit;
+}
+ 
+// Include config file
 include 'backend/database.php';
+ 
+$username = $password = "";
+$username_err = $password_err = "";
+ 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    if(empty(trim($_POST["email"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["email"]);
+    }
+    
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = md5(trim($_POST["password"]));
+    }
+    
+    if($_POST["email"] !='' && $_POST["password"] !=''){
+        $sql = "SELECT id, email, password FROM users WHERE email ='".$username."' and password='".$password."'";
+       
+              if ($result=mysqli_query($conn,$sql))
+              {
+              $rowcount=mysqli_num_rows($result);
+          
+             
+               } 
+                if($rowcount == 1){                    
+                    
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirect user to home page
+                            header("location: home.php");
+                        
+                } else{
+                    $username_err = "No account found with that username.";
+                }
+                mysqli_free_result($result);
+            
+            
+    }
+    
+    // Close connection
+    $mysqli->close();
+}
 ?>
+ 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>User Data</title>
+    <meta charset="UTF-8">
+    <title>Login</title>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -17,212 +75,62 @@ include 'backend/database.php';
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.9/validator.min.js"></script>
 	<script src="ajax/ajax.js"></script>
-</head>
-<style>
-	.error{
-	color:red;
-}
-</style>
-<body>
-    <div class="container">
-	<p id="success"></p>
-        <div class="table-wrapper">
-            <div class="table-title">
-                <div class="row">
-                    <div class="col-sm-6">
-						<h2>Manage <b>Users</b></h2>
-					</div>
-					<div class="col-sm-6">
-						<a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New User</span></a>
-						<a href="JavaScript:void(0);" class="btn btn-danger" id="delete_multiple"><i class="material-icons">&#xE15C;</i> <span>Delete</span></a>						
-					</div>
-                </div>
-            </div>
-            <table class="table table-striped table-hover">
-                <thead>
-                    <tr>
-						<th>
-							<span class="custom-checkbox">
-								<input type="checkbox" id="selectAll">
-								<label for="selectAll"></label>
-							</span>
-						</th>
-						<th>SL NO</th>
-                        <th>NAME</th>
-                        <th>EMAIL</th>
-						<th>PHONE</th>
-                        <th>CITY</th>
-                        <th>ACTION</th>
-                    </tr>
-                </thead>
-				<tbody>
-				
-				<?php
-				$result = mysqli_query($conn,"SELECT * FROM crud");
-					$i=1;
-					while($row = mysqli_fetch_array($result)) {
-				?>
-				<tr id="<?php echo $row["id"]; ?>">
-				<td>
-							<span class="custom-checkbox">
-								<input type="checkbox" class="user_checkbox" data-user-id="<?php echo $row["id"]; ?>">
-								<label for="checkbox2"></label>
-							</span>
-						</td>
-					<td><?php echo $i; ?></td>
-					<td><?php echo $row["name"]; ?></td>
-					<td><?php echo $row["email"]; ?></td>
-					<td><?php echo $row["phone"]; ?></td>
-					<td><?php echo $row["city"]; ?></td>
-					<td>
-						<a href="#editEmployeeModal" class="edit" data-toggle="modal">
-							<i class="material-icons update" data-toggle="tooltip" 
-							data-id="<?php echo $row["id"]; ?>"
-							data-name="<?php echo $row["name"]; ?>"
-							data-email="<?php echo $row["email"]; ?>"
-							data-phone="<?php echo $row["phone"]; ?>"
-							data-city="<?php echo $row["city"]; ?>"
-							title="Edit">&#xE254;</i>
-						</a>
-						<a href="#deleteEmployeeModal" class="delete" data-id="<?php echo $row["id"]; ?>" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" 
-						 title="Delete">&#xE872;</i></a>
-                    </td>
-				</tr>
-				<?php
-				$i++;
-				}
-				?>
-				</tbody>
-			</table>
-			
-        </div>
-    </div>
-	<!-- Add Modal HTML -->
-	<div id="addEmployeeModal" class="modal fade">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<form id="user_form" enctype="multipart/form-data" role="form" data-toggle="validator" novalidate="true">
-					<div class="modal-header">						
-						<h4 class="modal-title">Add User</h4>
-						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					</div>
-					<div class="modal-body">					
-						<div class="form-group">
-							<label>NAME</label>
-							<input type="text" id="name" name="name" class="form-control" required="required" data-error="Please enter your full name.">
-							<span class="error" style="display:none" id="filterErr" >Please Enter the Name<span>
-            			</div>
-						<div class="form-group">
-							<label>EMAIL</label>
-							<input type="email" id="email" name="email" class="form-control" required="required" data-error="Please enter a valid email.">
-							<span class="error" style="display:none" id="filteremailErr" >Please Enter the email<span>
-							<span class="error" style="display:none" id="filteremailformatErr" >Please Enter the valid email<span>
-						</div>
-						<div class="form-group">
-							<label>PHONE</label>
-							<input type="phone" id="phone" name="phone" class="form-control" required="required" data-error="Please enter your phone number">
-							<span class="error" style="display:none" id="filterphoneErr" >Please Enter the phone number<span>
-						</div>
-						
-						
-						<div class="form-group">
-							<input name="userImage" type="file" class="form-controller userimage" onchange="readURL(this.value);" /><br>
-							<img id="blah" src="#" alt="your image" style="display:none;"/>
-						</div>
-						
-										
-					</div>
-					<div class="modal-footer">
-					    <input type="hidden" value="1" name="type">
-						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-						<input value="Add" type="submit" class="btn btn-success"  id="btn-add">
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-	<!-- Edit Modal HTML -->
-	<div id="editEmployeeModal" class="modal fade">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<form id="update_form">
-					<div class="modal-header">						
-						<h4 class="modal-title">Edit User</h4>
-						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					</div>
-					<div class="modal-body">
-						<input type="hidden" id="id_u" name="id" class="form-control" required>					
-						<div class="form-group">
-							<label>Name</label>
-							<input type="text" id="name_u" name="name" class="form-control" required>
-						</div>
-						<div class="form-group">
-							<label>Email</label>
-							<input type="email" id="email_u" name="email" class="form-control" required>
-						</div>
-						<div class="form-group">
-							<label>PHONE</label>
-							<input type="phone" id="phone_u" name="phone" class="form-control" required>
-						</div>
-						<div class="form-group">
-							<label>City</label>
-							<input type="city" id="city_u" name="city" class="form-control" required>
-						</div>					
-					</div>
-					<div class="modal-footer">
-					<input type="hidden" value="2" name="type">
-						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-						<button type="button" class="btn btn-info" id="update">Update</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-	<!-- Delete Modal HTML -->
-	<div id="deleteEmployeeModal" class="modal fade">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<form>
-						
-					<div class="modal-header">						
-						<h4 class="modal-title">Delete User</h4>
-						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					</div>
-					<div class="modal-body">
-						<input type="hidden" id="id_d" name="id" class="form-control">					
-						<p>Are you sure you want to delete these Records?</p>
-						<p class="text-warning"><small>This action cannot be undone.</small></p>
-					</div>
-					<div class="modal-footer">
-						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-						<button type="button" class="btn btn-danger" id="delete">Delete</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
 	
-	<script type="text/javascript">
-$(document).ready(function (e) {
-   $("#uploadForm").on('submit',(function(e) {
-      e.preventDefault();
-      $.ajax({
-           url: "upload.php",
-         type: "POST",
-         data:  new FormData(this),
-         contentType: false,
-           cache: false,
-         processData:false,
-         success: function(data)
-          {
-            $("#targetLayer").html(data);
-          },
-              error: function() 
-          {
-          }            
-      });
-	}));
+	
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px;
+    margin: 0px auto;
+    margin-top: 100px;}
+	.error{color:red;}
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+        <form action="index.php" id="login" method="post" role="form" >
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="email" class="form-control" value="<?php echo $username; ?>" required>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+           
+        </form>
+    </div>    
+</body>
+<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.min.js"></script>
+<script>
+$(function() {
+	
+	$("#login").validate({
+	  rules: {
+		email: {
+		  required: true,
+		  email: true
+		},
+		password: {
+			required: true,
+			
+		  },
+		
+	  },
+	  messages: {
+		 email: "Please enter a valid email address",
+		 password: "Please enter the password"
+
+	  },
+	   submitHandler: function(form) {
+		form.submit();
+	   }
+	});
 });
 </script>
-</body>
-</html>                                		                            
+
+</html>
